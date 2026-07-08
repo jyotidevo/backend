152d1345fb86902be2979360997c06f04395f0b5
Jyoti Gupta <jyotigup2025@gmail.com>
2026-07-08 11:05:45 +0530

    Updated Server.js

--- Server.js
+++ Server.js
@@ -1,39 +1,27 @@
-
 const cors = require("cors");
 const express = require("express");
 const db = require("./Config/db");
-
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
+const UserRoute = require('./Routes/UserRoute');
+const StudentRoute = require('./Routes/StudentRoute');
+const ProductRoute = require('./Routes/ProductRoute');
 
-
-
-const userRoutes = require("./Routes/UserRoute");
 
 app.use(cors());
+
 app.use(express.json());
+app.use('/api/user', UserRoute);
+app.use('/api/student', StudentRoute);
+app.use('/api/product', ProductRoute);
+db.authenticate().then(() => {
+    console.log("Database connected successfully");
+    db.sync({ alter: true });    //to drop the existing table and create new one
+}).catch((err) => {
+    console.error("Error connecting to database:", err);
+})
 
-app.get("/", (req, res) => {
-    res.json({ message: "Backend API is running" });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-app.use("/api/users", userRoutes);
-
-const startServer = async () => {
-    try {
-        await db.authenticate();
-        console.log("Database connected successfully");
-
-        await db.sync();
-        console.log("Database synced successfully");
-
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (err) {
-        console.error("Error starting server:", err);
-        process.exit(1);
-    }
-};
-
-startServer();
